16d395f048fa00bdf558078d9a3ecb5f0de2298e
Helien-Dev <heliendevo@gmail.com>
2024-04-21 01:18:36 -0500

    Shipping form logic

--- store/views.py
+++ store/views.py
@@ -14,7 +14,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0, 'shipping': False}
         cartItems = order["get_cart_items"]
 
     products = Product.objects.all()
@@ -31,10 +31,10 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0, 'cart_items': cartItems}
+        order = {"get_cart_total": 0, "get_cart_items": 0, 'shipping': False}
         cartItems = order["get_cart_items"]
 
-    context = {"items": items, "order": order}
+    context = {"items": items, "order": order, 'cart_items': cartItems}
     return render(request, "store/cart.html", context)
 
 
@@ -47,10 +47,10 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0, 'cart_items': cartItems}
+        order = {"get_cart_total": 0, "get_cart_items": 0, 'shipping': False}
         cartItems = order["get_cart_items"]
 
-    context = {"items": items, "order": order}
+    context = {"items": items, "order": order, 'cart_items': cartItems}
     return render(request, "store/checkout.html", context)
 
 
